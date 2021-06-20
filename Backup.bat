bf0f29c63c1bd111a8d12305e33233b79b2b5d77
Jose Peral <jose.peral@dana.com>
2021-06-20 15:06:16 +0100

    release

--- Backup.bat
+++ Backup.bat
@@ -1,2 +1,2 @@
-7z a C:/Users/Sun/Desktop/BraveProfilesBackup.7z C:/Users/Sun/AppData/Local/BraveSoftware/*
+7z a -m0=lzma2 -mx=5 C:/Users/Sun/Desktop/BraveProfilesBackup.7z C:/Users/Sun/AppData/Local/BraveSoftware/*
 pause
