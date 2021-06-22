3122a70b0155dec28064d7ee397c43f33e9bf6a2
Europa <japeralsoler@gmail.com>
2021-06-22 19:10:49 +0100

    modified install script

--- Install.bat
+++ Install.bat
@@ -1,4 +1,4 @@
-cd ..
+cd %USERPROFILE%\Desktop
 xcopy ".\BS_Releases\desktop\*.*" /s /q
 xcopy "update & start.lnk" %USERPROFILE%"\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup" /s /q
 "icon locations on desktop.png"
