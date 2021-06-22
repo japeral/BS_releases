cb2e82432e1daefe587dc3edc3ca8183d4056c28
Callisto <japeralsoler@gmail.com>
2021-06-22 23:55:34 +0100

    fix

--- Install.bat
+++ Install.bat
@@ -1,4 +1,4 @@
 xcopy "%USERPROFILE%\Desktop\BS_Releases\desktop\*.*" "%USERPROFILE%\Desktop"  /s /q
-xcopy "update & start.lnk" %USERPROFILE%"\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup" /s /q
+xcopy "%USERPROFILE%\Desktop\update & start.lnk" "%USERPROFILE%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup" /q
 "%USERPROFILE%\Desktop\icon locations on desktop.png"
 del "%USERPROFILE%\Desktop\icon locations on desktop.png"
