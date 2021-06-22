f9804fb7f0a3b9a6920e93711ccae6380a19b7b1
VM-MOON <japeralsoler@gmail.com>
2021-06-22 16:34:50 +0200

    adding autostart

--- Install.bat
+++ Install.bat
@@ -1,3 +1,4 @@
 cd ..
 xcopy ".\BS_Releases\desktop\*.*" /s
 "icon locations on desktop.png"
+xcopy "update & start.lnk" %USERPROFILE%"\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"
