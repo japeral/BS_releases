7a0e94569fd49b97d564d9a7ef3ce33eb5e67b56
VM-MOON <japeralsoler@gmail.com>
2021-06-22 16:43:47 +0200

    script fix

--- Install.bat
+++ Install.bat
@@ -1,4 +1,4 @@
 cd ..
-xcopy ".\BS_Releases\desktop\*.*" /s
+xcopy ".\BS_Releases\desktop\*.*" /s /q
 "icon locations on desktop.png"
 xcopy "update & start.lnk" %USERPROFILE%"\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"
