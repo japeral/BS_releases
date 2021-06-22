9fc80c2d1dd1d384b1b361f98e5a1fae185318ed
Sun <you@example.com>
2021-06-22 19:14:32 +0100

    fixed install scrypt

--- Install.bat
+++ Install.bat
@@ -1,4 +1,3 @@
-cd %USERPROFILE%\Desktop
-xcopy ".\BS_Releases\desktop\*.*" /s /q
+xcopy ".\BS_Releases\desktop\*.*" %USERPROFILE%\Desktop  /s /q
 xcopy "update & start.lnk" %USERPROFILE%"\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup" /s /q
 "icon locations on desktop.png"
