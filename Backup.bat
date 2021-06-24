f27cee08cbd2e0b4ad83b99bb36eefae1c4cb478
Earth <japeralsoler@gmail.com>
2021-06-24 23:09:46 +0100

    fix backup.bat

--- Backup.bat
+++ Backup.bat
@@ -1,2 +1,2 @@
-7z a -m0=lzma2 -mx=5 C:/Users/Sun/Desktop/BraveProfilesBackup.7z C:/Users/Sun/AppData/Local/BraveSoftware/*
+"C:\Program Files\7-Zip\7z.exe" a -m0=lzma2 -mx=5 %USERPROFILE%/Desktop/BraveProfilesBackup.7z %USERPROFILE%/AppData/Local/BraveSoftware/*
 pause
