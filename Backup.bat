55bdc50555af984dd3468eea8780f474a9ddf5ad
Your Name <you@example.com>
2021-07-10 00:48:33 +0100

    generic push from rpi4

--- Backup.bat
+++ Backup.bat
@@ -1,6 +1,6 @@
 taskkill /F /IM brave.exe
 taskkill /F /IM Ccleaner64.exe
-xcopy %USERPROFILE%/Desktop/BS_Releases/balances*.xlsx %USERPROFILE%/AppData/Local/BraveSoftware/
-xcopy %USERPROFILE%/Desktop/BS_Releases/config*.xlsx %USERPROFILE%/AppData/Local/BraveSoftware/
+xcopy %USERPROFILE%\Desktop\BS_Releases\balances*.xlsx %USERPROFILE%\AppData\Local\BraveSoftware\
+xcopy %USERPROFILE%\Desktop\BS_Releases\config*.xlsx %USERPROFILE%\AppData\Local\BraveSoftware\
 "C:\Program Files\7-Zip\7z.exe" a -m0=lzma2 -mx=5 %USERPROFILE%/Desktop/BraveProfilesBackup.7z %USERPROFILE%/AppData/Local/BraveSoftware/*
 pause
