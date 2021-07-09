cd2f327d5709b706716246c99c4bd1b4b9acc8bf
Sharkon <japeralsoler@gmail.com>
2021-07-10 00:20:05 +0100

    generic push from rpi4

--- Backup.bat
+++ Backup.bat
@@ -1,2 +1,4 @@
+taskkill /F /IM brave.exe
+taskkill /F /IM Ccleaner64.exe
 "C:\Program Files\7-Zip\7z.exe" a -m0=lzma2 -mx=5 %USERPROFILE%/Desktop/BraveProfilesBackup.7z %USERPROFILE%/AppData/Local/BraveSoftware/*
 pause
