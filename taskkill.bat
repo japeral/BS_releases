b978a04f9584991fc4e4411d5f1df9a055ac1829
Jose Peral <jose.peral@dana.com>
2022-02-04 11:25:01 +0000

    taskkill script

---
+++ taskkill.bat
@@ -0,0 +1,6 @@
+runas /user:administrator "taskkill /F /IM brave.exe"
+runas /user:administrator "taskkill /F /IM Ccleaner64.exe/"
+taskkill /F /IM BraveCrashHandler.exe
+taskkill /F /IM BraveCrashHandler64.exe
+taskkill /F /IM BraveUpdate.exe
+taskkill /F /IM BSqueezer.exe
