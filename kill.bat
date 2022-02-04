9d0c11b6b8d7f011f98a09b36c0be03e57c61187
Jose Peral <jose.peral@dana.com>
2022-02-04 16:21:51 +0000

    public release

--- kill.bat
+++ kill.bat
@@ -1,5 +1,5 @@
 taskkill /F /IM brave.exe
-taskkill /F /IM Ccleaner64.exe/
+taskkill /F /IM Ccleaner64.exe
 taskkill /F /IM BraveCrashHandler.exe
 taskkill /F /IM BraveCrashHandler64.exe
 taskkill /F /IM BraveUpdate.exe
