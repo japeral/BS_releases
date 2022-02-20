e22e6908f45fe1115165af4744f105a2b01c6d82
Jose Peral <jose.peral@dana.com>
2022-02-20 21:17:19 +0000

    public release

--- update.bat
+++ update.bat
@@ -1,6 +1,6 @@
 cd %USERPROFILE%\Desktop\BS_Releases
 taskkill /F /IM BSqueezer.exe
-taskkill /F /IM brave.exe.exe
+taskkill /F /IM brave.exe
 REM git gc --quiet --prune=now --aggressive
 REM git gc
 del search.xlsx
